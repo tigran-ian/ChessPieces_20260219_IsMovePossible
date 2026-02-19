33748402a38d73f194550dc6dd5fb0b41589f894
Tigran <taginian1991@hotmail.com>
2026-02-19 20:32:46 +0400

    Task input

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Ունենալ Library Project որտեղ սահմանված կլինեն շախմատին վերաբերվող ապստրակցիները՝
+//Coordinate, Board, Folder որտեղ խաղաքարերը կլինեն և նրանք կունենան մեթոդներ
+//bool IsMovePossible(Coord startCoord, Coord endCoord), որը կվերադարձնի ամեն
+//քարի համար true եթե ներմուծված սկզբնակետից վերջնակետ մեկ քայլով տվյալ խաղաքարը
+//կարող է գնալ։
+//Ունենալ նաև Console Project որտեղ կկատարվեն ներմուծելու և տպելու գործառույթները։
+
